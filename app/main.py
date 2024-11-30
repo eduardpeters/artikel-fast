be5b42c581ff75c59c061ba3fa310ee254256e17
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-11-30 19:08:46 +0100

    Test basic get requests

--- app/main.py
+++ app/main.py
@@ -1,4 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+class Noun(BaseModel):
+    article: str
+    noun: str
+    is_plural: bool = False
+
+
+fake_db = {
+    1: Noun(article="Die", noun="Zeit"),
+    2: Noun(article="Die", noun="Zeiten", is_plural=True),
+    3: Noun(article="Der", noun="Tag"),
+    4: Noun(article="Die", noun="Tage", is_plural=True),
+}
 
 app = FastAPI()
 
@@ -6,3 +21,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/nouns/{noun_id}", response_model=Noun)
+async def get_noun_by_id(noun_id: int):
+    noun = fake_db.get(noun_id)
+    if not noun:
+        raise HTTPException(status_code=404, detail="Noun not found")
+    return noun
--- app/test_main.py
+++ app/test_main.py
@@ -9,3 +9,15 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_get_missing_noun_id():
+    response = client.get("/nouns/9999")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Noun not found"}
+
+
+def test_get_noun_by_id():
+    response = client.get("/nouns/1")
+    assert response.status_code == 200
+    assert response.json() == {"article": "Die", "noun": "Zeit", "is_plural": False}
