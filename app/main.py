d268c011c44cd5eb8227ce71f8ea0d255d01de24
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-12-08 22:04:34 +0100

    Add Article table

--- app/main.py
+++ app/main.py
@@ -5,14 +5,25 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
+class Article(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    article: str
+
+
 class NounBase(SQLModel):
     noun: str
 
 
 class Noun(NounBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    article: str
     is_plural: bool = False
+
+    article_id: int = Field(foreign_key="article.id")
+
+
+class NounResponse(NounBase):
+    id: int
+    article: str
 
 
 class NounQuestion(NounBase):
@@ -30,18 +41,30 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
     # Initial Seeding
-    seed_data = [
-        Noun(article="Die", noun="Zeit"),
-        Noun(article="Die", noun="Zeiten", is_plural=True),
-        Noun(article="Der", noun="Tag"),
-        Noun(article="Die", noun="Tage", is_plural=True),
+    seed_article_data = [
+        Article(id=1, article="Der"),
+        Article(id=2, article="Die"),
+        Article(id=3, article="Das"),
+    ]
+    seed_noun_data = [
+        Noun(article_id=2, noun="Zeit"),
+        Noun(article_id=2, noun="Zeiten", is_plural=True),
+        Noun(article_id=1, noun="Tag"),
+        Noun(article_id=2, noun="Tage", is_plural=True),
     ]
     with Session(engine) as session:
+        existing = session.exec(select(Article)).all()
+
+        if not existing:
+            for article in seed_article_data:
+                session.add(article)
+                session.commit()
+
         existing = session.exec(select(Noun).limit(1)).all()
         if existing:
             return
 
-        for noun in seed_data:
+        for noun in seed_noun_data:
             session.add(noun)
 
         session.commit()
@@ -75,14 +98,20 @@ async def get_nouns(session: SessionDep):
     return nouns
 
 
-@app.get("/nouns/{noun_id}", response_model=Noun)
+@app.get("/nouns/{noun_id}", response_model=NounResponse)
 async def get_noun_by_id(noun_id: int, session: SessionDep):
-    noun = session.get(Noun, noun_id)
+    result = session.exec(
+        select(Noun, Article).where(Noun.id == noun_id, Noun.article_id == Article.id)
+    ).first()
 
-    if not noun:
+    if not result:
         raise HTTPException(status_code=404, detail="Noun not found")
 
-    return noun
+    noun, article = result
+
+    response = NounResponse(id=noun.id, noun=noun.noun, article=article.article)
+
+    return response
 
 
 @app.get("/questions/{noun_id}", response_model=NounQuestion)
