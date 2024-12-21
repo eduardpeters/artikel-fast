782f4dd7579f61df0697c1b786f47ffbcac66b42
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-12-21 07:21:27 +0100

    Add session and client fixtures

--- app/test_main.py
+++ app/test_main.py
@@ -1,28 +1,53 @@
+import pytest
 from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
 
-from main import app
-
-client = TestClient(app)
+from main import app, get_session
 
 
-def test_read_main():
+@pytest.fixture(name="session")
+def session_fixture():
+    engine = create_engine(
+        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    def get_session_override():
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+
+# client = TestClient(app)
+
+
+def test_read_main(client: TestClient):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
 
-def test_get_nouns():
+def test_get_nouns(client: TestClient):
     response = client.get("/nouns")
     assert response.status_code == 200
 
 
-def test_get_missing_noun_id():
+def test_get_missing_noun_id(client: TestClient):
     response = client.get("/nouns/9999")
     assert response.status_code == 404
     assert response.json() == {"detail": "Noun not found"}
 
 
-def test_get_noun_by_id():
+def test_get_noun_by_id(client: TestClient):
     response = client.get("/nouns/1")
     assert response.status_code == 200
     assert response.json() == {
@@ -33,13 +58,13 @@ def test_get_noun_by_id():
     }
 
 
-def test_get_missing_question_noun_by_id():
+def test_get_missing_question_noun_by_id(client: TestClient):
     response = client.get("/questions/9999")
     assert response.status_code == 404
     assert response.json() == {"detail": "Question not found"}
 
 
-def test_get_question_noun_by_id():
+def test_get_question_noun_by_id(client: TestClient):
     response = client.get("questions/1")
     assert response.status_code == 200
     assert response.json() == {"id": 1, "noun": "Zeit"}
